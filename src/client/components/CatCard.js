162505ba6c8f039a06d7630a74ad5ef02ef3e65b
Denis McPhillips <denismcphillips@gmail.com>
2020-03-21 13:55:50 -0400

    nine completed

--- src/client/components/CatCard.js
+++ src/client/components/CatCard.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom' // hint, you may want to use this
+import React from "react";
+import { Link } from "react-router-dom"; // hint, you may want to use this
 
 // You have a bunch of freedom here to write this how you want
 // This is not really tested, other than what props you use here
 // Check AllCats specs for what is being checked
 
-const CatCard = (props) => {
-  return (
-    <div class='cat-card'>
-      YOUR CAT CARD CODE HERE
+const CatCard = props => {
+  return <div class="cat-card">YOUR CAT CARD CODE HERE</div>;
+};
 
-    </div>
-  )
-}
-
-export default CatCard
+export default CatCard;
--- src/client/components/SingleCat.js
+++ src/client/components/SingleCat.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import CatCard from './CatCard'
+import React from "react";
+import { connect } from "react-redux";
+import CatCard from "./CatCard";
 
 // why are we exporting so much stuff you might ask?
 // and I would say good question
@@ -12,31 +12,40 @@ import CatCard from './CatCard'
 // don't worry about it too much
 export class DisconnectedSingleCat extends React.Component {
   render() {
+    console.log(this.props);
     return (
-      <div className='single-cat'>
-
-      </div >
-    )
+      <div className="single-cat">
+        <h2>{this.props.name}</h2>
+        <img src={this.props.imageUrl}></img>
+        <ul className="toys">
+          {this.props.toyRatings.map(toy => (
+            <li>{toy.name}</li>
+          ))}
+        </ul>
+        <div className="friends">
+          <h3>Friends</h3>
+          {this.props.friends.map(friend => (
+            <CatCard key={friend.id} id={friend.id} />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
-export const mapStateToProps = (state) => {
-  return {
+export const mapStateToProps = state => {
+  return {};
+};
 
-  }
-}
-
-export const mapDispatchToProps = (dispatch) => {
-  return {
-
-  }
-}
+export const mapDispatchToProps = dispatch => {
+  return {};
+};
 
 // don't touch this line
 // but this is the component you probably want in most cases
 // so if you're using DisconnectedSingleCat somewhere else in your program
 // you're gonna have a bad time
-export default connect(mapStateToProps, mapDispatchToProps)(DisconnectedSingleCat)
-
-
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DisconnectedSingleCat);
